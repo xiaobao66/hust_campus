c21309af788e89df3f432e6d73a253001a566ebd
weidabao <wei.yucheng@foxmail.com>
2015-10-09 01:03:26 +0800

    更新了剩余电量文案显示

--- scripts/src/show.js
+++ scripts/src/show.js
@@ -1,13 +1,19 @@
 define(function(require, exports, module) {
     var util = require('./util'),
-        data = window.location.search.split("=")[1];
+        data = window.location.search.split("=")[1],
+        mes = {
+            elecFull: "电费满满哒,空调随便使用哦",
+            elecNearly: "要没电啦(o_o;)赶快去充电费哦!",
+            elecEnough: "电量还是够的,用空调不要心慌慌哦"
+        };
 
     data = decodeURIComponent(data);
     data = JSON.parse(data);
 
     $(function() {
         var average = util.averageElec(util.totalElec(data.data.recent), 7),
-            trendHeight = $(window).height() - $("#trend-title").offset().top - 25;
+            trendHeight = $(window).height() - $("#trend-title").offset().top - 25,
+            remain = data.data.remain;
 
         document.getElementById("change-dom").addEventListener("touchstart", function(e) {
             e.preventDefault();
@@ -17,7 +23,14 @@ define(function(require, exports, module) {
 
         $("#elec-trend").height(trendHeight);
         $("#aver-number").text(average);
-        $("#surplus-number").text(data.data.remain);
+        $("#surplus-number").text(remain);
+        if (remain > 100) {
+            $("#elec-info").text(mes.elecFull);
+        } else if (remain < 20) {
+            $("#elec-info").text(mes.elecNearly);
+        } else {
+            $("#elec-info").text(mes.elecEnough);
+        }
         util.surplusElec($("#surplus-elec"), data.data.remain, 360);
         var elecChart = echarts.init(document.getElementById('elec-trend'));
         util.sevenElec(elecChart, data.data.recent, trendHeight);
