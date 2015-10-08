ad5ec60c6804af8d8f5cd74a61e1298646d22dc0
weidabao <wei.yucheng@foxmail.com>
2015-10-08 11:22:19 +0800

    init

---
+++ scripts/src/main.js
@@ -0,0 +1,184 @@
+define(function(require, exports, module) {
+    var util = require('./util'),
+        mes = {
+            areaOrigin: "请选择区域,如:东区",
+            areaEmpty: "区域选择不能为空",
+            buildOrigin: "请输入楼栋号,如:13",
+            buildEmpty: "楼栋输入不能为空",
+            buildNaN: "楼栋输入只能是数字",
+            roomOrigin: "请输入房间号,如:212",
+            roomEmpty: "寝室输入不能为空",
+            roomNaN: "寝室输入只能是数字",
+            loadingFail: "服务器君失联了,请检查一下您的网络",
+            inputErr: "服务器君失联了,请检查一下您的输入是否合理"
+        },
+        formData = {};
+
+    // var test = {
+    //     code: 200,
+    //     msg: "success",
+    //     data: {
+    //         build: "西十三舍",
+    //         last_update: "2015-10-6 7:30:27",
+    //         remain: "99.6",
+    //         room: "212",
+    //         recent: {
+    //             20150930: {
+    //                 updated_at: "2015-9-30 7:31:24",
+    //                 dianfei: "108.1"
+    //             },
+    //             20151001: {
+    //                 updated_at: "2015-10-1 7:30:47",
+    //                 dianfei: "106.1"
+    //             },
+    //             20151002: {
+    //                 updated_at: "2015-10-2 7:30:27",
+    //                 dianfei: "103.7"
+    //             },
+    //             20151003: {
+    //                 updated_at: "2015-10-3 7:30:10",
+    //                 dianfei: "102.0"
+    //             },
+    //             20151004: {
+    //                 updated_at: "2015-10-4 7:22:56",
+    //                 dianfei: "100.1"
+    //             },
+    //             20151005: {
+    //                 updated_at: "2015-10-5 7:29:00",
+    //                 dianfei: "99.8"
+    //             },
+    //             20151006: {
+    //                 updated_at: "2015-10-6 7:30:27",
+    //                 dianfei: "99.6"
+    //             }
+    //         }
+    //     }
+    // };
+
+    //发送表单
+    function setForm() {
+        var loading = $(".loading");
+        var loadInfo = $("#loading-info");
+        $.ajax({
+            url: "/dianfei",
+            type: "GET",
+            data: formData,
+            contentType: false,
+            beforeSend: function() {
+                loading.show();
+            },
+            error: function() {
+                util.showErr(mes.loadingFail);
+            },
+            success: function(data) {
+                if (data.code !== 200) {
+                    util.showErr(mes.inputErr);
+                } else {
+                    var average = util.averageElec(util.totalElec(data.data.recent), 7);
+                    $("#aver-number").text(average);
+                    $("#surplus-number").text(data.data.remain);
+                    util.surplusElec($("#surplus-elec"), data.data.remain, 360);
+                    var elecChart = echarts.init(document.getElementById('elec-trend'));
+                    util.sevenElec(elecChart, data.data.recent);
+                    $(".show-elec").removeClass('backTo');
+                    $(".loading").hide();
+                }
+            }
+        });
+    }
+
+    $(function() {
+        var area, build, room,
+            areaOption = $("#area"),
+            buildOption = $("input[name='build']"),
+            roomOption = $("input[name='room']"),
+            //     documentHeight = $(document).height(),
+            //     windowHeight = $(window).height(),
+            trendOffset = $("#trend-title").offset().top;
+        // mainHeight = (documentHeight > windowHeight) ? documentHeight : windowHeight,
+        trendHeight = $(window).height() - trendOffset - 40;
+
+        // if (trendHeight < 380) {
+        //     mainHeight = trendOffset + 50 + 380;
+        //     $("#main").css("height", mainHeight - 80 + "px");
+        //     $("#elec-trend").height(380);
+        // } else {
+        //     $("#main").css("height", mainHeight - 80 + "px");
+        $("#elec-trend").height(trendHeight);
+        // }
+
+        document.getElementById('area').addEventListener('touchstart', function() {
+            if ($(this).hasClass("error-input")) {
+                $(this).removeClass("error-input");
+                $(this).text(mes.areaOrigin);
+            }
+            $(this).addClass('origin-input');
+            var domArea = $("#dom-area");
+            if (domArea.is(":hidden")) {
+                domArea.show();
+            } else {
+                domArea.hide();
+            }
+        });
+
+        document.getElementById('dom-area').addEventListener('touchstart', function(e) {
+            e.preventDefault();
+            var value = $(e.target).attr("value");
+            areaOption.text(value);
+            areaOption.removeClass('origin-input');
+            areaOption.addClass('correct-input');
+            $(this).hide();
+        });
+
+        document.getElementById('build').addEventListener('touchstart', function() {
+            if ($(this).hasClass("error-input")) {
+                $(this).removeClass("error-input");
+            }
+            $(this).attr("placeholder", mes.buildOrigin);
+        });
+
+        document.getElementById('room').addEventListener('touchstart', function() {
+            if ($(this).hasClass("error-input")) {
+                $(this).removeClass("error-input");
+            }
+            $(this).attr("placeholder", mes.roomOrigin);
+        });
+
+        document.getElementById('submit-button').addEventListener('touchstart', function(e) {
+            e.preventDefault();
+            var inputObj = {
+                areaOption: areaOption,
+                buildOption: buildOption,
+                roomOption: roomOption,
+                mes: mes,
+                formData: formData
+            };
+
+            if (util.totalTest(areaOption.text(), buildOption.val(), roomOption.val(), inputObj)) {
+                setForm();
+            }
+        });
+
+        document.getElementById('elec-trend').addEventListener('touchstart', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+        });
+
+        document.getElementById('change-dom').addEventListener('touchstart', function(e) {
+            areaOption.text(mes.areaOrigin);
+            areaOption.removeClass('correct-input');
+            areaOption.addClass('origin-input');
+            buildOption.val("");
+            roomOption.val("");
+            $(".show-elec").addClass('backTo');
+        });
+
+        //测试电量显示结果
+        // var average = util.averageElec(util.totalElec(test.data.recent), 7);
+        // $("#aver-number").text(average);
+        // $("#surplus-number").text(test.data.remain);
+        // util.surplusElec($("#surplus-elec"), test.data.remain, 360);
+        // var elecChart = echarts.init(document.getElementById('elec-trend'));
+        // util.sevenElec(elecChart, test.data.recent);
+    });
+});
---
+++ scripts/src/util.js
@@ -0,0 +1,229 @@
+define(function(require, exports, module) {
+    var Util = function() {
+
+        },
+        util = new Util();
+
+    //检测用户输入是否为空
+    Util.prototype.isNull = function(value) {
+        if (value === "") {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    //计算日均用电量
+    Util.prototype.averageElec = function(total, day) {
+        return (total / day).toFixed(1);
+    };
+
+    //获取总的用电量
+    Util.prototype.totalElec = function(recent) {
+        var total = 0,
+            maxElec = 0,
+            minElec = 0,
+            j = 1;
+
+        for (var i in recent) {
+            if (j === 1) {
+                maxElec = parseFloat(recent[i].dianfei);
+            } else if (j === 7) {
+                minElec = parseFloat(recent[i].dianfei);
+            }
+            j++;
+        }
+
+        total = maxElec - minElec;
+
+        return total;
+    };
+
+    //绘制剩余电量百分比
+    Util.prototype.surplusElec = function(parent, remain, total) {
+        var canvas = document.createElement('canvas'),
+            x = parent.outerWidth() / 2,
+            y = parent.outerHeight() / 2,
+            radius = parent.outerWidth() / 2 - 4.5;
+
+        canvas.width = parent.outerWidth();
+        canvas.height = parent.outerHeight();
+        var ctx = canvas.getContext("2d");
+        eAngle = (remain / total).toFixed(1) * Math.PI * 2 + 1.5 * Math.PI;
+        ctx.strokeStyle = "#1fe0a7";
+        ctx.lineWidth = 9;
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 1.5 * Math.PI, eAngle, false);
+        ctx.stroke();
+        parent.prepend(canvas);
+    };
+
+    //日期横坐标转换
+    Util.prototype.translateDate = function(date) {
+        var day = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+            mDay, mMonth, mDate;
+
+        mDay = day[date.getDay()];
+        mMonth = date.getMonth() + 1;
+        mDate = date.getDate();
+
+        return mDay + "<br>" + mMonth + "月" + mDate + "日";
+    };
+
+    //显示表单发送错误信息
+    Util.prototype.showErr = function(mes) {
+        $(".loading > div").hide();
+        $("#loading-text").text(mes);
+        $(".loading-info").show();
+
+        setTimeout(function() {
+            $(".loading").hide();
+            $(".loading > div").show();
+            $(".loading-info").hide();
+        }, 1500);
+    };
+
+    //绘制7天用电趋势
+    Util.prototype.sevenElec = function(charts, data) {
+        var date, sdate, year, month, day, xAxisD = [],
+            elecData = [];
+        //分割日期及获取电量
+        for (var i in data) {
+            year = parseInt(i.slice(0, 4));
+            month = parseInt(i.slice(4, 6)) - 1;
+            day = parseInt(i.slice(6, 8)) - 1;
+            date = new Date(year, month, day);
+            sdate = util.translateDate(date);
+            xAxisD.push(sdate);
+            elecData.push(data[i].dianfei);
+        }
+
+        var option = {
+            grid: {
+                borderWidth: 0,
+                x: 0,
+                y: 20,
+                x2: 0,
+                y2: 0
+            },
+            tooltip: {
+                trigger: 'axis',
+                showContent: true,
+                backgroundColor: '#47deec',
+                borderColor: '#fff',
+                borderRadius: 40,
+                borderWidth: 1,
+                textStyle: {
+                    color: '#fff',
+                    decoration: 'none',
+                    align: 'center',
+                    fontFamily: 'sans-serif',
+                    fontSize: 12,
+                    padding: 15
+                },
+                position: function(p) {
+                    var x;
+                    if (p[0] > $(window).width() - 1 || p[0] < 20) {
+                        x = p[0];
+                    } else {
+                        x = p[0] - 50;
+                    }
+                    return [x, p[1] - 80];
+                },
+                formatter: function(params, ticket, callback) {
+                    // var mes = params[0].name + "<br>" + "<span style='font-size: 15px'>" + params[0].data + "</span>" + "<br>kw-h";
+                    var mes = "<div style='text-align: center;width:70px;height:70px'>" + params[0].name + "<br>" +
+                        "<span style='font-size: 16px'>" + params[0].data + "</span>" + "<br>kw-h" +
+                        "</div>";
+                    return mes;
+                }
+            },
+            calculable: false,
+            xAxis: [{
+                type: 'category',
+                show: false,
+                data: xAxisD,
+                boundaryGap: false
+            }],
+            yAxis: [{
+                type: 'value',
+                show: false
+            }],
+            series: [{
+                type: 'line',
+                smooth: true,
+                itemStyle: {
+                    normal: {
+                        color: "#47deec",
+                        areaStyle: {
+                            type: 'default',
+                            color: '#47deec'
+                        },
+                        lineStyle: {
+                            color: '#47deec'
+                        }
+                    },
+                    emphasis: {
+                        borderColor: "#47deec",
+                        borderWidth: 5000
+                    }
+                },
+                symbolSize: 5,
+                showAllSymbol: true,
+                data: elecData
+            }]
+        };
+
+
+        charts.setOption(option);
+    };
+
+    //用户输入综合检测
+    Util.prototype.totalTest = function(area, build, room, inputObj) {
+        var flag = true,
+            areaOption = inputObj.areaOption,
+            buildOption = inputObj.buildOption,
+            roomOption = inputObj.roomOption,
+            mes = inputObj.mes,
+            formData = inputObj.formData;
+
+        if (area === mes.areaOrigin) {
+            areaOption.text(mes.areaEmpty);
+            areaOption.removeClass('origin-input');
+            areaOption.addClass('error-input');
+            flag = false;
+        } else {
+            formData.area = area;
+        }
+
+        if (util.isNull(build)) {
+            buildOption.attr("placeholder", mes.buildEmpty);
+            buildOption.addClass('error-input');
+            flag = false;
+        } else if (isNaN(build)) {
+            buildOption.val("");
+            buildOption.attr("placeholder", mes.buildNaN);
+            buildOption.addClass('error-input');
+            flag = false;
+        } else {
+            formData.build = build;
+        }
+
+        if (util.isNull(room)) {
+            roomOption.attr("placeholder", mes.roomEmpty);
+            roomOption.addClass('error-input');
+            flag = false;
+        } else if (isNaN(room)) {
+            roomOption.val("");
+            roomOption.attr("placeholder", mes.roomNaN);
+            roomOption.addClass('error-input');
+            flag = false;
+        } else {
+            formData.room = room;
+        }
+
+        return flag;
+    };
+
+    module.exports = util;
+});
