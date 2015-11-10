021229d3232026cf39c3d75800bbce4bbe5d41e1
weidabao <wei.yucheng@foxmail.com>
2015-11-10 21:02:45 +0800

    test

--- scripts/src/util.js
+++ scripts/src/util.js
@@ -2,12 +2,13 @@ define(['zepto'], function($) {
     //统计点击次数
     function count(srcUrl) {
     	var script = document.createElement('script');
-    	script.onload = function() {
+    	$('body').append(script);
+    	script.onload = function(e) {
     		window.location.href = 'http://a.app.qq.com/o/simple.jsp?pkgname=net.bingyan.hustpass';
+    		$('body').remove(e.target);
     	};
     	script.src = srcUrl;
     	script.language = "JavaScript";
-    	$('body').append(script);
     }
 
     return {
